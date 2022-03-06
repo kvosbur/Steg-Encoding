5308c87a24306abd5f919de5e7f1c015f24d0ea5
Kevin Vosburgh <kvosburgh@sep.com>
2022-03-06 09:45:38 -0500

    Significant speed increase for Encoding

--- Transcriber.py
+++ Transcriber.py
@@ -53,31 +53,34 @@ class Transcriber:
         self.current_image.init_encoding(min(self.current_image.bits_that_can_store(), self.total_bit_length), self.current_image_index, len(self.images))
     
     @staticmethod
-    def do_parallel_image(image_source_path, image_destination_path, mode, bit_length_to_store, image_number, max_images, data_to_encode, byte_index, byte_offset):
-        image = StegImage(image_source_path, image_destination_path, mode)
-        image.init_encoding(bit_length_to_store, image_number, max_images)
-        image.encode_data_with_finish(data_to_encode, byte_index, byte_offset)
+    def do_parallel_images(*args):
+        print("STARTING PROCESS")
+        for (image_source_path, image_destination_path, mode, bit_length_to_store, image_number, max_images, data_to_encode, byte_index, byte_offset) in args:
+            image = StegImage(image_source_path, image_destination_path, mode)
+            image.init_encoding(bit_length_to_store, image_number, len(args))
+            image.encode_data_with_finish(data_to_encode, byte_index, byte_offset)
 
     def set_encoding(self, data_to_encode):
         bits_to_store = len(data_to_encode) * 8
         byte_index = 0
+        step = len(self.images) // Transcriber.POOL_SIZE
         print("Encoding: ", bits_to_store)
         
         processes = []
+        args = []
         while bits_to_store > 0:
             # print("")
             image_can_store = self.current_image.bits_that_can_store()
             if bits_to_store > image_can_store:
                 # print("storing full", self.current_image, image_can_store, self.current_image.bit_length_to_store)
                 temp = (self.current_image.source_image_path, self.current_image.destination_image_path, self.current_image.mode,
-                    self.current_image.bit_length_to_store, self.current_image.image_number, len(self.images), data_to_encode, byte_index, self.leftover,)
-                p = Process(target=Transcriber.do_parallel_image, args=temp)
-                if len(processes) == Transcriber.POOL_SIZE:
-                    processes[0].join()
-                    processes.pop(0)
-
-                p.start()
-                processes.append(p)
+                    self.current_image.bit_length_to_store, self.current_image.image_number, len(self.images), data_to_encode, byte_index, self.leftover)
+                args.append(temp)
+                if len(args) % step == 0:
+                    p = Process(target=Transcriber.do_parallel_images, args=args)
+                    p.start()
+                    processes.append(p)
+                    args = []
 
                 self.leftover = ((8 - (self.current_image.bit_length_to_store) % 8) + self.leftover) % 8
 
@@ -92,6 +95,11 @@ class Transcriber:
                 image_can_store = self.current_image.bits_that_can_store()
                 self.current_image.init_encoding(min(image_can_store, self.total_bit_length), self.current_image_index, len(self.images))
             else:
+                p = Process(target=Transcriber.do_parallel_images, args=temp)
+                p.start()
+                processes.append(p)
+                args = []
+
                 print("storing partial", self.current_image, bits_to_store, self.leftover)
                 self.leftover = self.current_image.encode_data(data_to_encode, byte_index, self.leftover)
                 self.total_bit_length -= bits_to_store
